b58c3b6c6a8330a8a19a6594b3cff43642349bdf
Higor Fernandes <higorsfernandes11@gmail.com>
2024-08-07 23:03:01 -0300

    Lista de espera de hospital.
    Sistema de Lista de espera de hospital.

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 let lista = [];
 let programa = "aberto";
 
-while (programa === "aberto"){
-    let opcoes = window.prompt("1) Adicionar nome \n 2) Exibir lista \n 3) Proximo nome \n 4) Fechar programa");
+function acrescentarPaciente(nome, sobrenome, idade, urgencia){
+    let pessoa = Object.create({});
+    pessoa.nome = nome;
+    pessoa.sobrenome = sobrenome;
+    pessoa.idade = idade;
+    pessoa.urgencia = urgencia;
+    return pessoa;
+}
 
+while (programa == "aberto"){
+    let opcoes = window.prompt("1) Adicinar Paciente \n 2) Exibir Lista \n 3) Proximo Nome \n 4) Fechar Programa");
     if (opcoes == 1){
-        let acrescentar = "s";
-        while (acrescentar == "s"){
-            let pessoa = {};
-            pessoa.nome = window.prompt("Nome do paciente");
-            lista.push(pessoa);
-            acrescentar = window.prompt("Deseja acrescentar mais um nome? (s/n)");
-            if (acrescentar == "n"){
-                break;
-            }
+        let nome = window.prompt("Primeiro Nome:");
+        let sobrenome = window.prompt("Sobrenome: ");
+        let idade = parseInt(window.prompt("Idade do paciente"));
+        let urgencia = window.prompt("Paciente precisa ser atendido urgentemente? (s/n)");
+
+        let paciente = acrescentarPaciente(nome, sobrenome, idade, urgencia);
+        
+        if(idade > 75 || urgencia == "s"){
+            lista.unshift(paciente);
+        }else {
+            lista.push(paciente);
         }
     } else if (opcoes == 2){
-        for (let x = 0; x < lista.length; x ++){
-            window.alert(lista[x].nome);
+        for(let x = 0; x < lista.length; x++){
+            window.alert(`Nome: ${lista[x].nome} \n Sobrenome: ${lista[x].sobrenome} \n Idade: ${lista[x].idade} \n Urgencia: ${lista[x].urgencia}`)
         }
     } else if (opcoes == 3){
-        window.alert("O proximo paciente é " + lista[0].nome);
+        window.alert(`Proximo Paciente: \n ${lista[0].nome} ${lista[0].sobrenome}`);
         lista.shift();
-    } else if (opcoes == 4){
+    }else if (opcoes == 4){
         break;
     }
 }
